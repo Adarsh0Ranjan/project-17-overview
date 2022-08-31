347ef4016461cf2acff79bcf7ae248c0f8dc8db3
adarsh <adarsh0ranjan@gmail.com>
2022-08-31 08:54:04 +0530

    Initial Commit

---
+++ project-17-ovverview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project-17-ovverview
+//
+//  Created by Roro Solutions on 31/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ project-17-ovverview/project_17_ovverviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_17_ovverviewApp.swift
+//  project-17-ovverview
+//
+//  Created by Roro Solutions on 31/08/22.
+//
+
+import SwiftUI
+
+@main
+struct project_17_ovverviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
